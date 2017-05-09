5fd64da5182bd9154c3936d69cd6d380b93031ec
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2017-05-08 20:59:33 -0500

    Updated text and link to events list page

--- clubstrapblocks/templates/frontintrocontent.tpl.php
+++ clubstrapblocks/templates/frontintrocontent.tpl.php
@@ -3,7 +3,7 @@
     <span class="fa fa-circle fa-stack-2x font-awesome-round-bg-green">‌</span>
     <span aria-hidden="true" class="fa fa-calendar fa-stack-1x">‌‌</span>
   </span>
-  <h3><a class="border-bottom-dotted" href="events">Events</a></h3>
+  <h3><a class="border-bottom-dotted" href="category/events">Events</a></h3>
   <p>Checkout information about our weekly meetups, upcoming tech talks &amp; competitions.</p>
 </div>
 
@@ -13,7 +13,7 @@
     <span aria-hidden="true" class="fa fa-television fa-stack-1x">‌‌</span>
   </span>
   <h3><a class="border-bottom-dotted" href="videos">Videos</a></h3>
-  <p>You can find all the video tutorials made by our faculty and member over here.</p>
+  <p>Here you can find our collection of videos that our members have shared.</p>
 </div>
 
 <div class="col-md-4 text-center">
@@ -22,5 +22,5 @@
     <span aria-hidden="true" class="fa fa-code-fork fa-stack-1x">‌‌</span>
   </span>
   <h3><a class="border-bottom-dotted" href="projects">Projects</a></h3>
-  <p>Here you can get complete list of projects currently being developed by the club.</p>
+  <p>This is the place where you can find the complete list of projects currently being developed by the club.</p>
 </div>
