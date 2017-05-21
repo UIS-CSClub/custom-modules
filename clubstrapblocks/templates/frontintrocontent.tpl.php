abf19f3591f289dc9b0a3b4df9a4bd7783157756
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2017-05-21 01:09:04 -0500

    Changed icon used for videos section

--- clubstrapblocks/templates/frontintrocontent.tpl.php
+++ clubstrapblocks/templates/frontintrocontent.tpl.php
@@ -10,7 +10,7 @@
 <div class="col-md-4 text-center">
   <span class="fa-stack fa-5x">
     <span class="fa fa-circle fa-stack-2x font-awesome-round-bg-pink">‌</span>
-    <span aria-hidden="true" class="fa fa-television fa-stack-1x">‌‌</span>
+    <span aria-hidden="true" class="fa fa-play-circle fa-stack-1x">‌‌</span>
   </span>
   <h3><a class="border-bottom-dotted" href="videos">Videos</a></h3>
   <p>Here you can find our collection of videos that our members have shared.</p>
