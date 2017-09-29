6d4ef71d7cd5569a5bb2411681cbdbd2e2b8fdac
Bhavyanshu Parasher <bhavyanshu.spl@gmail.com>
2017-09-28 21:20:08 -0500

    Updated social links

--- clubstrapblocks/src/Plugin/Block/FollowIcons.php
+++ clubstrapblocks/src/Plugin/Block/FollowIcons.php
@@ -26,11 +26,11 @@ class FollowIcons extends BlockBase {
   public function printHtml() {
 
     $result = '<h3>Join Us On</h3>';
-    $result .= '<span class="fa-stack fa-lg icon-facebook">‌<span class="fa fa-square fa-stack-2x">‌</span> <span class="fa fa-facebook fa-stack-1x">‌</span></span>';
+    $result .= '<a href="https://www.facebook.com/csclub.uis/" target="_blank"><span class="fa-stack fa-lg icon-facebook">‌<span class="fa fa-square fa-stack-2x">‌</span> <span class="fa fa-facebook fa-stack-1x">‌</span></span></a>';
     $result .= '<span class="fa-stack fa-lg icon-twitter">‌ <span class="fa fa-square fa-stack-2x">‌</span> <span class="fa fa-twitter fa-stack-1x">‌</span> </span>';
     $result .= '<span class="fa-stack fa-lg icon-youtube">‌ <span class="fa fa-square fa-stack-2x">‌</span> <span class="fa fa-youtube fa-stack-1x">‌</span> </span>';
-    $result .= '<span class="fa-stack fa-lg icon-github">‌ <span class="fa fa-square fa-stack-2x">‌‌</span> <span class="fa fa-github fa-stack-1x">‌‌</span> </span>';
-    $result .= '<span class="fa-stack fa-lg icon-rss">‌ <span class="fa fa-square fa-stack-2x">‌‌‌</span> <span class="fa fa-rss fa-stack-1x">‌‌‌</span> </span>';
+    $result .= '<a href="https://github.com/UIS-CSClub" target="_blank"><span class="fa-stack fa-lg icon-github">‌ <span class="fa fa-square fa-stack-2x">‌‌</span> <span class="fa fa-github fa-stack-1x">‌‌</span> </span></a>';
+    $result .= '<a href="/rss.xml" target="_blank"><span class="fa-stack fa-lg icon-rss">‌ <span class="fa fa-square fa-stack-2x">‌‌‌</span> <span class="fa fa-rss fa-stack-1x">‌‌‌</span> </span></a>';
     return $result;
   }
 
